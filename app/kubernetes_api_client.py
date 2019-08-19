c189bee5509c83c7e8feec8e715c50ce1a1d237f
TanZhengWei <YITU-INC\zhengwei.tan@yitu-inc.intra>
2019-08-19 10:14:32 +0800

    Revert api_query back to variable

--- app/kubernetes_api_client.py
+++ app/kubernetes_api_client.py
@@ -10,18 +10,17 @@ api_client = client.ApiClient(config)
 api = client.CoreV1Api(api_client)
 extensions_api = client.ExtensionsV1beta1Api(api_client)
 
-def api_query():
-    if os.environ.get('API_RESOURCE') == 'pod':
-        return api.list_pod_for_all_namespaces()
-    elif os.environ.get('API_RESOURCE') == 'pvc':
-        return api.list_persistent_volume_claim_for_all_namespaces()
-    elif os.environ.get('API_RESOURCE') == 'node':
-        return api.list_node()
-    elif os.environ.get('API_RESOURCE') == 'service':
-        return api.list_service_for_all_namespaces()
-    elif os.environ.get('API_RESOURCE') == 'ingress':
-        return extensions_api.list_ingress_for_all_namespaces()
-    elif os.environ.get('API_RESOURCE') == 'deployment':
-        return extensions_api.list_deployment_for_all_namespaces()
-    else:
-        raise Exception('API_RESOURCE environment variable not assigned or invalid.')
+if os.environ.get('API_RESOURCE') == 'pod':
+    api_query = api.list_pod_for_all_namespaces
+elif os.environ.get('API_RESOURCE') == 'pvc':
+    api_query = api.list_persistent_volume_claim_for_all_namespaces
+elif os.environ.get('API_RESOURCE') == 'node':
+    api_query = api.list_node
+elif os.environ.get('API_RESOURCE') == 'service':
+    api_query = api.list_service_for_all_namespaces
+elif os.environ.get('API_RESOURCE') == 'ingress':
+    api_query = extensions_api.list_ingress_for_all_namespaces
+elif os.environ.get('API_RESOURCE') == 'deployment':
+    api_query = extensions_api.list_deployment_for_all_namespaces
+else:
+    raise Exception('API_RESOURCE environment variable not assigned or invalid.')
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhengwei143/kubernetes-resource-monitor
+
+go 1.12
